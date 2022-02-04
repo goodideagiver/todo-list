9b48b251ce63a8ae5e72b94b3e3aa8064a91d3d5
prplblck <kariurek@gmail.com>
2022-02-04 14:40:27 +0100

    remove list entry

--- src/script/app.js
+++ src/script/app.js
@@ -5,6 +5,9 @@ const createDeleteButton = () => {
 	const deleteButton = document.createElement('button');
 	deleteButton.classList.add('delete-entry-button');
 	deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+	deleteButton.addEventListener('click', () => {
+		deleteButton.closest('.list-entry').remove();
+	});
 	return deleteButton;
 };
 
