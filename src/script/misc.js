12a3f13ce02ed9df683d1c9f7ee6b6220ed3379e
prplblck <kariurek@gmail.com>
2022-02-05 13:15:49 +0100

    favicon

---
+++ src/script/misc.js
@@ -0,0 +1,38 @@
+const saveChanges = () => {
+	const editModeButtons = document.querySelectorAll('.edit-mode-buttons');
+	if (editModeButtons) {
+		editModeButtons.forEach((element) => {
+			element.firstElementChild.click();
+		});
+	}
+	const lists = [];
+	mainListNodeContainer.childNodes.forEach((list) => {
+		const listObj = {
+			listName: list.querySelector('header h2').innerText,
+			listEntries: getEntriesText(list),
+		};
+		lists.push(listObj);
+	});
+	console.log('Saving lists', lists);
+	localStorage.setItem('lists', JSON.stringify(lists));
+};
+
+const addElementsToContainer = (container, elementsArray) => {
+	elementsArray.forEach((element) => {
+		container.appendChild(element);
+	});
+};
+
+const createCustomNode = (elementType, object) => {
+	const button = document.createElement(elementType);
+	if (object) {
+		object.callbackFunc ? button.addEventListener('click', object.callbackFunc) : 0;
+		object.customClassname ? button.classList.add(object.customClassname) : 0;
+		object.icon ? (button.innerHTML = object.icon) : 0;
+		object.placeholder ? (button.placeholder = object.placeholder) : 0;
+		object.text ? (button.innerText = object.text) : 0;
+		object.input ? (button.value = object.input) : 0;
+		object.saveOnClick ? button.addEventListener('click', saveChanges) : 0;
+	}
+	return button;
+};
